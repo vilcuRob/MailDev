365ffb9e9f722f00c2ab803acdfc04a49207bc59
Robert Vilcu <VILCUR@domesticandgeneral.com>
2016-11-07 15:12:28 +0000

    Gulp inline css task

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,10 @@ var express = require('express'),
     gulp = require('gulp'),
     handlebars = require('gulp-compile-handlebars'),
     rename = require('gulp-rename'),
-    sass   = require('gulp-sass'),
-    inlineCss = require('gulp-inline-css'),
-    concat = require('gulp-concat');
-
+    sass = require('gulp-sass'),
+    inlineCss = require('gulp-inline-css');
 
 var i = 0;
-var ii = 0;
 
 // Checks if gulp is runned with new or start args
 if (args.new == undefined && args.start == undefined) {
@@ -25,49 +22,49 @@ if (args.new == undefined && args.start == undefined) {
 }
 
 /******
-**
-* Gulp general tasks
-* 
-*
-*/
+ **
+ * Gulp general tasks
+ * 
+ *
+ */
 gulp.task('build_css', function () {
+    
+    //reset i
+    i = 0;
 
-    gulp.src(['./templates/'+template+'/style.scss'])
+    gulp.src(['./templates/' + template + '/style.scss'])
         .pipe(sass())
-        .pipe(gulp.dest('./dist/'+template+'/'))
-        .on('end', function() {
-           gulp.start(['build_html0']);
+        .pipe(gulp.dest('./dist/' + template + '/'))
+        .on('end', function () {
+            gulp.start(['build_html0']);
         });
-   
+
 }).task('watch', function () {
-    
-    // Watch and build html
-    gulp.watch(['./partials/*/*.hbs','./partials/*/*.json', './templates/*/*.hbs', './templates/*/*.json'],['build_html0']);
-   
-    // Watch and build css
-    gulp.watch(['./partials/*/*.scss', './templates/*/*.scss'],['build_css']);
-    
+
+    // Watch and build css and html
+    gulp.watch(['./partials/**', './templates/**'], ['build_css', 'build_html0']);
+
 }).task('default', function () {});
 
 /******
-**
-* Check if template directory exists
-* and if not creates a new folder structure
-*
-*/
+ **
+ * Check if template directory exists
+ * and if not creates a new folder structure
+ *
+ */
 if (!fs.existsSync('./templates/' + args.start)) {
     if (args.new !== undefined) {
         if (!fs.existsSync('./templates/' + args.new)) {
 
             // New template name
             template = args.new;
-            
+
             mkdirp('./templates/' + args.new + '/', function () {
                 fs.writeFile('./templates/' + args.new + '/style.scss', '');
                 fs.writeFile('./templates/' + args.new + '/data.json', '{\r\n\t"versions":\r\n\t\t[\r\n\t\t\t{\r\n\t\t\t\t\r\n\t\t\t}\r\n\t\t]\r\n}');
-                fs.writeFile('./templates/' + args.new + '/index.hbs', ''); 
+                fs.writeFile('./templates/' + args.new + '/index.hbs', '');
             });
-  
+
         } else {
             console.log('Template folder name already exists!');
             process.exit();
@@ -80,75 +77,71 @@ if (!fs.existsSync('./templates/' + args.start)) {
 
     // Existent template name
     template = args.start;
-    // Start build and watch tasks
-    gulp.start(['build_css', 'watch']);
-}
-
-var jh = require('./json_helpers');
-var totalPartials = jh.getJsonPartials(template).length;
-
-// Create empty html files so that the gulp task
-// inline_css would not fail
-mkdirp('./dist/' + template + '/', function () {
-    for(t = 0; t < totalPartials; t++){
-        fs.writeFile('./dist/' + template + '/v'+t+'.html', ''); 
-    }
-});
-
-var buildHtml = function () {
-
-    templateData = jh.getJsonPartials(template)[i];
-
-    // Gulp HBS compiler options    
-    options = {
-        ignorePartials: false,
-        // Partials paths batch array
-        batch: jh.getPartialsArrayPath(),
-        helpers: require('./hbs_helpers')
-    }
-
-    return gulp.src('templates/' + template + '/index.hbs')
-        .pipe(handlebars(templateData, options))
-        .pipe(rename('v'+i+'.html'))
-        .pipe(gulp.dest('dist/'+template))
-        .on('end', function() {
-            if(i < totalPartials - 1){
-                i++;
-                gulp.start(['build_html'+i]);
-            }else{
-                gulp.start(['inline_css']);
-            }
-        });
-}
-
-gulp.task('inline_css', function () {
     
-    return gulp.src(['dist/' + template + '/v0.html', 'dist/' + template + '/v1.html', 'dist/' + template + '/v2.html'])
-        .pipe(inlineCss({
-            	applyStyleTags: false,
-            	applyLinkTags: false,
-            	removeStyleTags: false,
-            	removeLinkTags: false
-        }))
-        .pipe(gulp.dest('dist/'+template));
-});
+    var jh = require('./json_helpers');
+    var totalPartials = jh.getJsonPartials(template).length;
 
+    
+    var buildHtml = function () {
 
-for(var x = 0; x < jh.getJsonPartials(template).length; x++){
-    gulp.task('build_html'+x, buildHtml);
+        templateData = jh.getJsonPartials(template)[i];
+
+        // Gulp HBS compiler options    
+        options = {
+            ignorePartials: false,
+            // Partials paths batch array
+            batch: jh.getPartialsArrayPath(),
+            helpers: require('./hbs_helpers')
+        }
+
+        return gulp.src('templates/' + template + '/index.hbs')
+            .pipe(handlebars(templateData, options))
+            .pipe(rename('v' + i + '.html'))
+            .pipe(gulp.dest('dist/' + template))
+            .on('end', function () {
+                if (i < totalPartials - 1) {
+                    i++;
+                    gulp.start(['build_html' + i]);
+                }else{
+                    gulp.start(['inline_css']);
+                }
+            });
+    }
+
+    gulp.task('inline_css', function () {
+
+        var distHtmlFiles = [];
+        for(var i = 0; i < totalPartials; i++){
+            distHtmlFiles.push('dist/' + template + '/v'+i+'.html');
+        }
+        return gulp.src(distHtmlFiles)
+            .pipe(inlineCss({
+                applyStyleTags: true,
+                applyLinkTags: true,
+                removeStyleTags: false,
+                removeLinkTags: false
+            }))
+            .pipe(gulp.dest('dist/' + template));
+    });
+
+    for (var x = 0; x < jh.getJsonPartials(template).length; x++) {
+        gulp.task('build_html' + x, buildHtml);
+    }
+    
+    // Start build and watch tasks
+    gulp.start(['build_css', 'build_html0', 'watch']);
 }
-
 
 /******
-**
-* Start server on port :1111
-* and serve the static index.html file
-*
-*/
+ **
+ * Start server on port :1111
+ * and serve the static index.html file
+ *
+ */
 
 app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/' + template +'/v0.html'));
+    res.sendFile(path.join(__dirname + '/dist/' + template + '/v0.html'));
 }).get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/' + template + req.originalUrl + '.html'));
 }).listen(1111, function () {
--- json_helpers.js
+++ json_helpers.js
@@ -1,80 +1,76 @@
 var fs = require('fs');
 
-function fileExists(filePath)
-{
-    try
-    {
+function fileExists(filePath) {
+    try {
         return fs.statSync(filePath).isFile();
-    }
-    catch (err)
-    {
+    } catch (err) {
         return false;
     }
 }
 
-module.exports = (function() {
-    
+module.exports = (function () {
+
     return {
 
         // Gets all the JSON from the partials
         // Used inside a particular index.hbs template
-        getJsonPartials: function(template){
-            
-            if(fileExists('./templates/'+template+'/index.hbs')){
+        getJsonPartials: function (template) {
 
-            var partial = [];
-            var partials = '';
+            if (fileExists('./templates/' + template + '/index.hbs')) {
 
-            // Get all partials used in the index.hbs file
-            var indexHbs = fs.readFileSync('./templates/'+template+'/index.hbs', 'utf-8').replace(/ /g,'');
-            // Read all the partials used inside hbs template
-            var indexHbs = indexHbs.split('{{>');
-            // Partial.push all the partial names used
-            for (var i = 1; i < indexHbs.length; i++) {
-                partial.push(indexHbs[i].split('}}')[0]);
-            }
-            // Read the json files of all the partials used
-            for (var i = 0; i < partial.length; i++) {
-                partials += fs.readFileSync('./partials/'+partial[i]+'/'+partial[i]+'.json', 'utf-8')+',';
-            }
+                var partial = [];
+                var partials = '';
 
-            // Return an object containing all partials used
-            var partials = JSON.parse('{'+partials.substring(0, partials.length - 1)+'}');
+                // Get all partials used in the index.hbs file
+                var indexHbs = fs.readFileSync('./templates/' + template + '/index.hbs', 'utf-8').replace(/ /g, '');
+                // Read all the partials used inside hbs template
+                var indexHbs = indexHbs.split('{{>');
+                // Partial.push all the partial names used
+                for (var i = 1; i < indexHbs.length; i++) {
+                    partial.push(indexHbs[i].split('}}')[0]);
+                }
+                // Read the json files of all the partials used
+                for (var i = 0; i < partial.length; i++) {
+                    partials += fs.readFileSync('./partials/' + partial[i] + '/' + partial[i] + '.json', 'utf-8') + ',';
+                }
 
-            // Get template data.json 
-            var dataJson = JSON.parse(fs.readFileSync('./templates/'+template+'/data.json', 'utf-8'));
+                // Return an object containing all partials used
+                var partials = JSON.parse('{' + partials.substring(0, partials.length - 1) + '}');
 
-            // Merges the partials Json with the template data.json
-            // And assigns the data.json as the one that overrides the partial's json data
-            var templateData = new Array;
-            for (var i = 0; i < dataJson.versions.length; i++) {
-                var data = {};
-                Object.assign(data, partials, dataJson.versions[i]);
-                templateData.push(data);
-            }
+                // Get template data.json 
+                var dataJson = JSON.parse(fs.readFileSync('./templates/' + template + '/data.json', 'utf-8'));
 
-            // Return object containing final 
-            // json to generate distribution
-            return templateData;   
-                
-            }else{
-                
+                // Merges the partials Json with the template data.json
+                // And assigns the data.json as the one that overrides the partial's json data
+                var templateData = new Array;
+                for (var i = 0; i < dataJson.versions.length; i++) {
+                    var data = {};
+                    Object.assign(data, partials, dataJson.versions[i]);
+                    templateData.push(data);
+                }
+
+                // Return object containing final 
+                // json to generate distribution
+                return templateData;
+
+            } else {
+
                 return {};
             }
         },
-        
-        getPartialsArrayPath: function(){
-            
+
+        getPartialsArrayPath: function () {
+
             var template_path = './partials/';
             var files = fs.readdirSync(template_path);
             var partial_paths = [];
-            for(var i = 0; i < files.length; i++){
-                partial_paths.push('./partials/'+files[i]+'/');
+            for (var i = 0; i < files.length; i++) {
+                partial_paths.push('./partials/' + files[i] + '/');
             }
             return partial_paths;
-                                   
+
         }
-                  
+
     };
-    
+
 })();
